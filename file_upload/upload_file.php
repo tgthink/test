e8fe9d8407cbf58905c16a33ea0c4a1273c54f8c
tgthink <tgthink@live.cn>
2016-01-19 09:08:09 +0800

    添加文件上传测试
    添加文件上传测试

---
+++ file_upload/upload_file.php
@@ -0,0 +1,70 @@
+
+<?php
+
+// if ($_FILES["file"]["error"] > 0)
+//   {
+//   echo "Error: " . $_FILES["file"]["error"] . "<br />";
+//   }
+// else
+//   {
+//   echo "Upload: " . $_FILES["file"]["name"] . "<br />";
+//   echo "Type: " . $_FILES["file"]["type"] . "<br />";
+//   echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
+//   echo "Stored in: " . $_FILES["file"]["tmp_name"];
+//   }
+
+// if ((($_FILES["file"]["type"] == "image/gif")
+// || ($_FILES["file"]["type"] == "image/jpeg")
+// || ($_FILES["file"]["type"] == "image/pjpeg"))
+// && ($_FILES["file"]["size"] < 20000))
+//   {
+//   if ($_FILES["file"]["error"] > 0)
+//     {
+//     echo "Error: " . $_FILES["file"]["error"] . "<br />";
+//     }
+//   else
+//     {
+//     echo "Upload: " . $_FILES["file"]["name"] . "<br />";
+//     echo "Type: " . $_FILES["file"]["type"] . "<br />";
+//     echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
+//     echo "Stored in: " . $_FILES["file"]["tmp_name"];
+//     }
+//   }
+// else
+//   {
+//   echo "Invalid file";
+//   }
+define('ROOT',dirname(__FILE__).'/');
+if ((($_FILES["file"]["type"] == "image/gif")
+|| ($_FILES["file"]["type"] == "image/jpeg")
+|| ($_FILES["file"]["type"] == "image/pjpeg"))
+&& ($_FILES["file"]["size"] < 200000))
+  {
+  if ($_FILES["file"]["error"] > 0)
+    {
+    echo "Return Code: " . $_FILES["file"]["error"] . "<br />";
+    }
+  else
+    {
+    echo "Upload: " . $_FILES["file"]["name"] . "<br />";
+    echo "Type: " . $_FILES["file"]["type"] . "<br />";
+    echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
+    echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br />";
+
+    if (file_exists("upload/" . $_FILES["file"]["name"]))
+      {
+      echo $_FILES["file"]["name"] . " already exists. ";
+      }
+    else
+      {
+      move_uploaded_file($_FILES["file"]["tmp_name"],
+      ROOT."upload/" . $_FILES["file"]["name"]);
+      echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
+      }
+    }
+  }
+else
+  {
+  echo "Invalid file";
+  }
+?>
