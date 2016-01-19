980675fe388280c1e28826882c4bfd059a7f09ee
tgthink <tgthink@live.cn>
2016-01-19 18:11:45 +0800

    添加微店前端构建
    添加微店前端构建

---
+++ webpack-gulp/weidian/gulpfile.js
@@ -0,0 +1,112 @@
+/*
+ * npm install gulp-less --save-dev
+ * 
+ */
+var gulp = require('gulp');	//基础库
+var less = require('gulp-less');	//编译less
+var clean = require('gulp-clean');	//清空文件夹
+var autoprefixer = require('gulp-autoprefixer');	//自动处理浏览器前缀
+
+var paths = {
+	src_html: 		"./src/**/*.html",
+	src_php: 		"./src/**/*.php",
+	src_json: 		"./src/**/*.json",
+	src_txt: 		"./src/**/*.txt",
+	src_js: 		"./src/js/**/*.js",
+	src_js_total: 	"./src/js/**/*.*",
+	src_css: 		"./src/css/**/*.css",
+	src_less: 		"./src/css/**/*.less",
+
+	src_js_css: "src/js/**/*.css",
+	src_js_less: "src/js/**/*.less",
+
+	src_images: 'src/images/**/*',
+	src_pic: 'src/pic/**/*',
+
+	dist_url: 		"./dist",
+	dist_html: 		"./dist/**/*.html",
+	dist_js: 		"./dist/js",
+	dist_css: 		"./dist/css",
+
+	// main_js: "main.js",
+	// dist_images: "dist/images",
+	// dist_pic: 'dist/pic',
+	// rev_css: "dist/rev/css"
+};
+var handleHtmlAry = [paths.src_html, paths.src_json, paths.src_php, paths.src_txt];
+// HTML处理
+gulp.task('html', function() {
+    gulp.src(handleHtmlAry)
+    .pipe(gulp.dest(paths.dist_url));
+});
+// 处理样式
+gulp.task('styles', function() {
+	/*js内css start*/
+	gulp.src([paths.src_js_less])
+	.pipe(less())
+	.pipe(autoprefixer('> 1%', 'IE 7'))
+	.pipe(gulp.dest(paths.dist_js));
+	gulp.src(['src/js/**/*.css'])
+	.pipe(gulp.dest(paths.dist_js));
+	/*js内css end*/
+	//处理less
+	gulp.src([paths.src_less])
+	.pipe(less())
+	.pipe(autoprefixer('> 1%', 'IE 7'))
+	.pipe(gulp.dest(paths.dist_css));
+	//处理普通css
+	gulp.src([paths.src_css])
+	.pipe(gulp.dest(paths.dist_css));
+});
+// 图片
+gulp.task('images', function() {  
+  return gulp.src(paths.src_images)
+    /*.pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))*/
+    .pipe(gulp.dest(paths.dist_images));
+    //.pipe(notify({ message: 'Images task complete' }));
+});
+gulp.task('pic', function() {  
+  return gulp.src(paths.src_pic)
+    /*.pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))*/
+    .pipe(gulp.dest(paths.dist_pic));
+    //.pipe(notify({ message: 'Images task complete' }));
+});
+// 脚本
+gulp.task('scripts', function() {
+	gulp.src([paths.src_js_total])
+    .pipe(gulp.dest(paths.dist_js));
+});
+// 清理
+gulp.task('clean', function() {
+  //, paths.dist_images, paths.dist_pic aths.dist_js
+	gulp.src([paths.dist_js, paths.dist_html, paths.dist_css], {read: false})
+	.pipe(clean());
+});
+// 定义默认任务
+gulp.task('default', ['clean'], function() {
+    gulp.start('styles', 'scripts', 'images', 'pic');
+    //gulp.start('styles', 'scripts', 'images', 'pic');
+    setTimeout(function(){
+      gulp.start('html');
+    }, 1000);
+});
+
+// 看守
+gulp.task('watch', function() {
+  // 由于某些js控件或库的样式放置在js同空间名目录
+  // 看守所有css档
+  gulp.watch([paths.src_css, paths.src_less, paths.src_js_css, paths.src_js_less], ['styles', 'html']);
+  //gulp.watch(paths.src_css, ['styles']);
+
+  // 看守所有.js档
+  gulp.watch([paths.src_js], ['scripts']);
+  // 看守所有图片档
+  gulp.watch(paths.src_images, ['images']);
+  gulp.watch(paths.src_pic, ['pic']);
+
+  // 监听html
+  gulp.watch(handleHtmlAry, function(event) {
+      gulp.run('html');
+  });
+
+});
---
+++ webpack-gulp/weidian/readme.txt
@@ -0,0 +1,12 @@
+﻿说明 start
+	images 放项目类图片、图标
+	pic 放开发过程中的产品图片
+
+说明 end
+
+使用到的插件 start
+	一般dom操作引用zepto，如引入的插件不兼容则引入jquery
+	Zepto v1.0rc1 : http://www.zeptojs.cn/
+	jquery-2.2.0.min.js : 
+	
+使用到的插件 end
